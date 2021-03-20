436bc5ca22dca5295f1187bf2d5a21c912461df1
sarakarlsen <71293547+sarakarlsen@users.noreply.github.com>
2021-03-20 13:26:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ let carts = document.querySelectorAll("#cart-button");
 
 for (let i=0; i < carts.length; i++) {
     carts[i].addEventListener("click", () => {
-        console.log("added to cart")
+        cartNumbers();
     })
+
+}
+
+function cartNumbers() {
+    let productNumbers = localStorage.getItem("cartNumbers");
+    console.log(productNumbers);
+    console.log(typeof productNumbers);
+    localStorage.setItem("cartNumbers", 1);
 
 }
