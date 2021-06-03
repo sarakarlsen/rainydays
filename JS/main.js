bb305b17a25da5f9e4613a5a1bd575910f8e2272
sarakarlsen <71293547+sarakarlsen@users.noreply.github.com>
2021-06-03 14:23:01 +0200

    api

--- JS/main.js
+++ JS/main.js
@@ -1,159 +1,132 @@
-
 const productsTest = document.querySelector(".product-row");
 
-const url = "https://flower-power.site/wp-json/wc/store/products";
+const url =
+  "http://rainydays-website.tsh.olx.mybluehost.me/test/wc/store/products";
 
 async function fetchProducts() {
-    try {
-        const response = await fetch(url);
-        const details = await response.json();
+  try {
+    const response = await fetch(url);
+    const details = await response.json();
 
-        console.log(details);
+    console.log(details);
 
-        details.forEach(function(result) {
-            productsTest.innerHTML += `
+    details.forEach(function (result) {
+      productsTest.innerHTML += `
             <div class="column">
              <a href="product.html?id=${result.id}">
             <img src="${result.images[0].src}"</img></a>
             <div>${result.name}</div>
             <a href="product.html?id=${result.id}" class="call-to-action" id="cart-button">Read more</a>
-            </div>`
-        });
-
-    
-    }
-
-    catch(error) {
-        console.log(error);
-    }
+            </div>`;
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 fetchProducts();
 
-
-
-
-
-
 let carts = document.querySelectorAll("#cart-button");
 
 let products = [
-    {
-        name: "Blue jacket",
-        tag: "bluejacket",
-        price: 250,
-        inCart: 0
-    },
-    {
-        name: "Grey jacket",
-        tag: "greyjacket",
-        price: 225,
-        inCart: 0
-    },
-    {
-        name: "Black jacket",
-        tag: "blackjacket",
-        price: 200,
-        inCart: 0
-    }
-
-
-
-
+  {
+    name: "Blue jacket",
+    tag: "bluejacket",
+    price: 250,
+    inCart: 0,
+  },
+  {
+    name: "Grey jacket",
+    tag: "greyjacket",
+    price: 225,
+    inCart: 0,
+  },
+  {
+    name: "Black jacket",
+    tag: "blackjacket",
+    price: 200,
+    inCart: 0,
+  },
 ];
 
 for (let i = 0; i < carts.length; i++) {
-    carts[i].addEventListener("click", () => {
-        cartNumbers(products[i]);
-        totalPrice(products[i])
-    })
-
+  carts[i].addEventListener("click", () => {
+    cartNumbers(products[i]);
+    totalPrice(products[i]);
+  });
 }
 
 function onLoadCartNumbers() {
-    let productNumbers = localStorage.getItem("cartNumbers");
+  let productNumbers = localStorage.getItem("cartNumbers");
 
-    if (productNumbers) {
-        document.querySelector(".cart span").textContent = productNumbers;
-
-    }
+  if (productNumbers) {
+    document.querySelector(".cart span").textContent = productNumbers;
+  }
 }
 
 function cartNumbers(product) {
+  let productNumbers = localStorage.getItem("cartNumbers");
 
-    let productNumbers = localStorage.getItem("cartNumbers");
+  productNumbers = parseInt(productNumbers);
 
+  if (productNumbers) {
+    localStorage.setItem("cartNumbers", productNumbers + 1);
+    document.querySelector(".cart span").textContent = productNumbers + 1;
+  } else {
+    localStorage.setItem("cartNumbers", 1);
+    document.querySelector(".cart span").textContent = 1;
+  }
 
-    productNumbers = parseInt(productNumbers);
-
-
-    if (productNumbers) {
-
-        localStorage.setItem("cartNumbers", productNumbers + 1);
-        document.querySelector(".cart span").textContent = productNumbers + 1;
-
-    } else {
-        localStorage.setItem("cartNumbers", 1);
-        document.querySelector(".cart span").textContent = 1;
-    }
-
-    setItems(product);
+  setItems(product);
 }
 
 function setItems(product) {
-    let cartItems = localStorage.getItem("productsInCart");
-    cartItems = JSON.parse(cartItems);
-    console.log("My cartItems are", cartItems);
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+  console.log("My cartItems are", cartItems);
 
-    if (cartItems != null) {
-        if (cartItems[product.tag] == undefined) {
-            cartItems = {
-                ...cartItems,
-                [product.tag]: product
-            }
-        }
-        cartItems[product.tag].inCart += 1;
-
-    } else {
-        product.inCart = 1;
-        cartItems = {
-            [product.tag]: product
-        }
+  if (cartItems != null) {
+    if (cartItems[product.tag] == undefined) {
+      cartItems = {
+        ...cartItems,
+        [product.tag]: product,
+      };
     }
+    cartItems[product.tag].inCart += 1;
+  } else {
+    product.inCart = 1;
+    cartItems = {
+      [product.tag]: product,
+    };
+  }
 
-
-    localStorage.setItem("productsInCart", JSON.stringify
-        (cartItems));
-
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems));
 }
 
 function totalPrice(product) {
+  let cartPrice = localStorage.getItem("totalPrice");
 
-    let cartPrice = localStorage.getItem("totalPrice");
+  console.log("My cartCost is", cartPrice);
+  console.log(typeof cartPrice);
 
-    console.log("My cartCost is", cartPrice);
-    console.log(typeof cartPrice);
-
-    if (cartPrice != null) {
-        cartPrice = parseInt(cartPrice);
-        localStorage.setItem("totalPrice", cartPrice + product.price);
-    } else {
-
-        localStorage.setItem("totalPrice", product.price);
-    }
+  if (cartPrice != null) {
+    cartPrice = parseInt(cartPrice);
+    localStorage.setItem("totalPrice", cartPrice + product.price);
+  } else {
+    localStorage.setItem("totalPrice", product.price);
+  }
 }
 
 function showCart() {
-    let cartItems = localStorage.getItem("productsInCart")
-    cartItems = JSON.parse(cartItems);
-    let productContainer = document.querySelector(".products");
-    let cartPrice = localStorage.getItem("totalPrice");
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+  let productContainer = document.querySelector(".products");
+  let cartPrice = localStorage.getItem("totalPrice");
 
-
-    if (cartItems && productContainer) {
-        productContainer.innerHTML = "";
-        Object.values(cartItems).map(example => {
-            productContainer.innerHTML += `
+  if (cartItems && productContainer) {
+    productContainer.innerHTML = "";
+    Object.values(cartItems).map((example) => {
+      productContainer.innerHTML += `
             <div class="product">
             <i class="fal fa-times-circle"></i>
             <img src= "./images/${example.tag}.jpg">
@@ -169,16 +142,10 @@ function showCart() {
             ${example.inCart * example.price}
             </div>
     
-            `
+            `;
+    });
 
-                ;
-
-
-
-
-        });
-
-        productContainer.innerHTML += `
+    productContainer.innerHTML += `
    <div class="basketTotalContainer">
    <h4">
    Total price
@@ -187,14 +154,9 @@ function showCart() {
      $${cartPrice}.00
      </h4>
 
-        `
-
-
-    }
-
+        `;
+  }
 }
-
-
 
 onLoadCartNumbers();
 showCart();
--- JS/product.js
+++ JS/product.js
@@ -8,66 +8,32 @@ const id = params.get("id");
 
 console.log(id);
 
-
-const url_api = "https://flower-power.site/wp-json/wc/store/products/" + id;
+const url_api =
+  "http://rainydays-website.tsh.olx.mybluehost.me/test/wp-json/wc/store/products/" +
+  id;
 
 console.log(url_api);
 
 async function fetchInfo() {
-    try {
-        const response = await fetch(url_api);
-        const info = await response.json();
-        console.log(info);
+  try {
+    const response = await fetch(url_api);
+    const info = await response.json();
+    console.log(info);
 
-        createHTML(info);
-    }
-
-    catch(error) {
-        console.log(error);
-        detailContainer.innerHTML = "An error has occured";
-    }
+    createHTML(info);
+  } catch (error) {
+    console.log(error);
+    detailContainer.innerHTML = "An error has occured";
+  }
 }
 
 fetchInfo();
 
 function createHTML(info) {
-    detailContainer.innerHTML = `
+  detailContainer.innerHTML = `
     <h1>${info.name}</h1>
                       <img src="${info.images[0].src}"</img>
                                   <p>${info.description}<p>
                                   <h2 style=padding:20px;>$ ${info.prices.price}</h2>
-                                  <a href="#" class="call-to-action" id="cart-button">Add to bag</a>`
+                                  <a href="#" class="call-to-action" id="cart-button">Add to bag</a>`;
 }
-
-
-
-
-const responseThree = await fetch(url);
-        const testAgain = await responseThree.json();
-
-        console.log(testAgain);
-
-    
-
-  
-
-              testAgain.forEach(function(testing){
-
-            
-      
-                postsContainerTwo.innerHTML += `<div class="categories-container">
-                <ul>
-                <a href="details.html<h3>${testing.title.rendered}</h3>
-                <img src="${testing.jetpack_featured_media_url}"</img>
-                <button class="button">READ POST</buttton>
-                </ul>
-                </div>`
-      
-                console.log(testing.id);
-              
-              })
-      
-              ; 
-
-
-              callApiAgain();
